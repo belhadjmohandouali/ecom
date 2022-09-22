fe86f27a99b9388db58d9cf11d6f85f9dccab1a9
mbelhadj <mbelhadj@jems-group.com>
2022-09-22 17:32:40 +0200

    liste produits, fiche d'un produit, filtre par categories, recherche

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Classe\Search;
 use App\Entity\Product;
+use App\Form\SearchType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
@@ -17,12 +20,36 @@ class ProductController extends AbstractController
     }
 
     #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $products = $this->entityManager->getRepository(Product::class)->findAll();
+
+
+        $search = new Search();
+        $form = $this->createForm(SearchType::class, $search);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $products = $this->entityManager->getRepository(Product::class)->findWithSearch($search);
+        }else{
+            $products = $this->entityManager->getRepository(Product::class)->findAll();
+        }
+
 
         return $this->render('product/index.html.twig', [
             'products' => $products,
+            'form' => $form->createView(),
         ]);
     }
+
+    #[Route('/product/{slug}', name: 'product')]
+    public function show($slug)
+    {
+        $product = $this->entityManager->getRepository(Product::class)->findOneBySlug($slug);
+        if (!$product){
+            return $this->redirectToRoute('app_product');
+        }
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+
 }
